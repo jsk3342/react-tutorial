bad7289615abb171c606536f7a5ffffe31c66056
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2022-12-17 03:01:17 +0900

    Add: context 완료

---
+++ context-tutorial/src/components/SelectColors.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ColorConsumer } from '../contexts/color';
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+const SelectColors = () => {
+    return (
+        <div>
+            <h2>생삭을 선택하세요</h2>
+            <ColorConsumer>
+                {
+                    ({ actions }) => (
+                        <div style={{display: 'flex'}}>
+                            {colors.map(color => (
+                                <div key={color}
+                                    onClick={() => actions.setColor(color)}
+                                    onContextMenu={e => {
+                                        e.preventDefault();
+                                        actions.setSubcolor(color)
+                                    } }
+                                style={{
+                                    background: color,
+                                    width: '24px',
+                                    height: '24px',
+                                    cursor: 'pointer'
+                                } } />
+                                ))}
+                        </div>
+                    )
+                }
+            </ColorConsumer>
+        </div>
+    );
+};
+
+export default SelectColors;
