4765ff8ad6893d4549dc498ee15bce8a6ad41c06
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2022-12-18 20:19:19 +0900

    Add: front

---
+++ 27/blog-frontend/src/pages/PostListPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import HeaderContainer from '../containers/common/HeaderContainer';
+import PostListContainer from '../containers/posts/PostListContainer';
+import PaginationContainer from '../containers/posts/PaginationContainer';
+import Button from '../components/common/Button';
+
+const PostListPage = () => {
+  return (
+    <>
+      <Button>버튼</Button>
+      {/* <HeaderContainer />
+      <PostListContainer />
+      <PaginationContainer /> */}
+    </>
+  );
+};
+
+export default PostListPage;
